d4fe34e000a266018b33a92465aade6c8b6781ad
ihyavorskyi <ihor201324@gmail.com>
2021-03-25 20:13:44 +0200

    Edit RegisterUser method

--- TransactionManagementAPI/Features/Commands/UsersCRUD/RegisterUser.cs
+++ TransactionManagementAPI/Features/Commands/UsersCRUD/RegisterUser.cs
@@ -5,13 +5,14 @@ using System.Threading;
 using System.Threading.Tasks;
 using TouristClubApi.Data.Models;
 using TransactionManagementAPI.Data;
+using TransactionManagementAPI.Data.DTOs;
 using TransactionManagementAPI.Data.Models;
 
 namespace TransactionManagementAPI.Features.Commands.UsersCRUD
 {
     public class RegisterUser
     {
-        public class Command : IRequest<bool>
+        public class Command : IRequest<Response>
         {
             public RegisterModel Model { get; set; }
 
@@ -21,23 +22,21 @@ namespace TransactionManagementAPI.Features.Commands.UsersCRUD
             }
         }
 
-        public class Handler : IRequestHandler<RegisterUser.Command, bool>
+        public class Handler : IRequestHandler<RegisterUser.Command, Response>
         {
-            private readonly AppDbContext _context;
             private readonly UserManager<User> _userManager;
 
-            public Handler(AppDbContext context, UserManager<User> userManager)
+            public Handler(UserManager<User> userManager)
             {
-                _context = context;
                 _userManager = userManager;
             }
 
-            public async Task<bool> Handle(Command command, CancellationToken cancellationToken)
+            public async Task<Response> Handle(Command command, CancellationToken cancellationToken)
             {
                 var userExist = await _userManager.FindByNameAsync(command.Model.UserName);
                 if (userExist != null)
                 {
-                    return false;
+                    return new Response() { Status = "Error", Message = "User already exist" };
                 }
 
                 var user = new User()
@@ -50,10 +49,10 @@ namespace TransactionManagementAPI.Features.Commands.UsersCRUD
                 var result = await _userManager.CreateAsync(user, command.Model.Password);
                 if (!result.Succeeded)
                 {
-                    return false;
+                    return new Response() { Status = "Error", Message = "Failed to create user" };
                 }
 
-                return true;
+                return new Response() { Status = "Successfully", Message = "User created successfully" };
             }
         }
     }
